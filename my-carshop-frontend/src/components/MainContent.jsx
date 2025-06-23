7e87fffff53d7be326090919fe124801c008f4d1
Khramenko Vladislav Vladimirovich <vladhromenko@gmail.com>
2025-06-23 13:22:29 +0300

    fixes

--- my-carshop-frontend/src/components/MainContent.jsx
+++ my-carshop-frontend/src/components/MainContent.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function MainContent() {
   const [cars, setCars] = useState([]);
@@ -8,11 +11,11 @@ export default function MainContent() {
 
   useEffect(() => {
     fetch('/api/cars/')
-      .then((res) => res.json())
+      .then(res => res.json())
       .then(setCars);
 
     fetch('/api/categories/')
-      .then((res) => res.json())
+      .then(res => res.json())
       .then(setCategories);
   }, []);
 
@@ -20,11 +23,47 @@ export default function MainContent() {
     navigate(`/cars?category=${catId}`);
   };
 
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    adaptiveHeight: true
+  };
+
   return (
     <div className="wrap">
       <div className="content">
+
+        {/* Слайдер */}
+        <div className="slider-wrapper theme-default">
+          <Slider {...sliderSettings}>
+            <div>
+              <img src="/images/slider1.jpg" alt="Слайд 1" />
+            </div>
+            <div>
+              <a href="http://dev7studios.com" target="_blank" rel="noreferrer">
+                <img src="/images/slider2.jpg" alt="Слайд 2" />
+              </a>
+            </div>
+            <div>
+              <img src="/images/slider3.jpg" alt="Слайд 3" />
+            </div>
+            <div>
+              <img src="/images/slider4.jpg" alt="Слайд 4" />
+              <div className="caption">
+                <strong>Различные</strong> типы <em>автомобилей</em>
+              </div>
+            </div>
+          </Slider>
+        </div>
+
+        {/* Машины */}
         <div className="section group">
-          {cars.slice(0, 3).map((car) => (
+          {cars.slice(0, 3).map(car => (
             <div className="grid_1_of_4 images_1_of_4" key={car.id}>
               <h3>Модель {car.title}</h3>
               <Link to={`/cars/${car.id}`}>
@@ -49,17 +88,18 @@ export default function MainContent() {
           </div>
         </div>
 
+        {/* Категории */}
         <div className="section group">
           <div className="rsidebar span_1_of_3">
             <h3>Категории</h3>
-            {categories.map((cat) => (
+            {categories.map(cat => (
               <p
                 className="top"
                 key={cat.id}
                 style={{ display: 'flex', alignItems: 'center', marginBottom: '10px', cursor: 'pointer' }}
                 onClick={() => handleCategoryClick(cat.id)}
               >
-                <span style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
+                <span style={{ display: 'flex', alignItems: 'center', color: 'inherit' }}>
                   <img src="/images/art-pic1.jpg" alt="" style={{ marginRight: '10px', width: '25px' }} />
                   {cat.name}
                 </span>
